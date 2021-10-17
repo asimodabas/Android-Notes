deefe4c54c640aa68fc980cd0c0c31cff70c69e6
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-18 02:03:34 +0300

    Slider-SeekBar added

--- app/src/main/java/com/asimodabas/backstack/ActivityC.kt
+++ app/src/main/java/com/asimodabas/backstack/ActivityC.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_c.*
 
 class ActivityC : AppCompatActivity() {
@@ -35,13 +36,37 @@ class ActivityC : AppCompatActivity() {
 
                 if (b) {
                     Log.e("ToggleButton Açık", "ON")
+                    val girdi  = b.toString()
+                    textView6Durum.text = girdi.toString()
+
 
                 } else {
                     Log.e("ToggleButton Kapalı", "OFF")
+                    val girdi  = b.toString()
+                    textView6Durum.text = girdi.toString()
+                    Log.e("Switch Kapalı","OFF")
 
                 }
             }
         }
+
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                val seekbardurum = p1.toString()
+                textView9.text=seekbardurum
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
 
     }
 }
