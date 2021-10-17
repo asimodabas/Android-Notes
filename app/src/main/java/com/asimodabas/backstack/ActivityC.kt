33f2b0961051e411e87c9d7a3b3e68e4c965ff0b
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-18 01:15:05 +0300

    Android Notes

--- app/src/main/java/com/asimodabas/backstack/ActivityC.kt
+++ app/src/main/java/com/asimodabas/backstack/ActivityC.kt
@@ -3,6 +3,7 @@ package com.asimodabas.backstack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_c.*
 
 class ActivityC : AppCompatActivity() {
@@ -14,5 +15,33 @@ class ActivityC : AppCompatActivity() {
             startActivity(Intent(this@ActivityC,ActivityD::class.java))
 
         }
+
+        switch1.setOnCheckedChangeListener { compoundButton, b ->
+
+            if (b){
+                Log.e("Switch Açık","ON")
+              val girdi  = b.toString()
+                textView6Durum.text = girdi.toString()
+
+            }
+
+                else{
+                val girdi  = b.toString()
+                textView6Durum.text = girdi.toString()
+                Log.e("Switch Kapalı","OFF")
+
+            }
+            toggleButton.setOnCheckedChangeListener { compoundButton, b ->
+
+                if (b) {
+                    Log.e("ToggleButton Açık", "ON")
+
+                } else {
+                    Log.e("ToggleButton Kapalı", "OFF")
+
+                }
+            }
+        }
+
     }
 }
--- app/src/main/java/com/asimodabas/backstack/ActivityD.kt
+++ app/src/main/java/com/asimodabas/backstack/ActivityD.kt
@@ -3,6 +3,8 @@ package com.asimodabas.backstack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_d.*
 
 class ActivityD : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,31 @@ class ActivityD : AppCompatActivity() {
 
     }
 
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+    //SEÇİLEN SEÇİMİ textView8 de string olarak göstermiyor.
+
     override fun onBackPressed() {
         val intent = Intent(this@ActivityD,MainActivity::class.java)
-
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
+
+
+        buttonSecim.setOnClickListener {
+            val javadurum = checkBoxJava.isChecked
+            val kotlindurum = checkBoxKotlin.isChecked
+            val barcelonadurum = radioButtonBarcelona.isChecked
+            val galatasaraydurum = radioButtonGalatasaray.isChecked
+
+        textView8.text = javadurum.toString()
+        textView8.text = kotlindurum.toString()
+        textView8.text = barcelonadurum.toString()
+        textView8.text = galatasaraydurum.toString()
+
+        }
 
     }
 }
