d1ed1fef99440b29a77e1a9bedb1e285e759c5b8
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-24 01:29:36 +0300

    revive

--- app/src/main/java/com/asimodabas/backstack/ActivityB.kt
+++ app/src/main/java/com/asimodabas/backstack/ActivityB.kt
@@ -42,11 +42,15 @@ class ActivityB : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_ayarlar -> {
-                Toast.makeText(this,"Ayarlara basildi",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Ayarlara basildi", Toast.LENGTH_SHORT).show()
                 return true
             }
             R.id.action_cikis -> {
-                Toast.makeText(this,"Cikisa basildi",Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Cikisa basildi", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.action_bilgi -> {
+                Toast.makeText(this, "Bilgiye basildi", Toast.LENGTH_SHORT).show()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
