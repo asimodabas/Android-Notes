411e843ea8307f864966b19395a7fc011f9e9f0d
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-10-27 00:07:33 +0300

    Update main page

--- app/src/main/java/com/asimodabas/backstack/MainActivity.kt
+++ app/src/main/java/com/asimodabas/backstack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.asimodabas.backstack
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,10 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sp = getSharedPreferences("GirisSayici", Context.MODE_PRIVATE)
+        var sayac = sp.getInt("sayac", 0)
+        val editor = sp.edit()
+        val newValue = sayac + 1
+        editor.putInt("sayac", newValue)
+        editor.apply()
+        textsayac.text = "Programa Giriş Sayısı :$sayac"
 
         fab.setOnClickListener {
-        Toast.makeText(this,"Floating Action Button Aktif",Toast.LENGTH_SHORT).show()
-    }
+            Toast.makeText(this, "Floating Action Button Aktif", Toast.LENGTH_SHORT).show()
+        }
 
         val fm = supportFragmentManager
         val ft = fm.beginTransaction()
@@ -73,7 +81,6 @@ class MainActivity : AppCompatActivity() {
 
             }
             popup.show()
-
 
 
         }
