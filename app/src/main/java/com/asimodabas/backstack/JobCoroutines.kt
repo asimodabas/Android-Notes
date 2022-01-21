35c5af6ee0ce451630b4e47e37e06c31e56af5dd
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-21 18:02:21 +0300

    coroutines job

---
+++ app/src/main/java/com/asimodabas/backstack/JobCoroutines.kt
@@ -0,0 +1,28 @@
+package com.asimodabas.backstack
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(){
+
+
+    runBlocking {
+
+        val myJob = launch {
+            delay(2000)
+            println("job")
+            val secondJob = launch {
+                println("job 2")
+            }
+        }
+
+        myJob.invokeOnCompletion {
+            println("my job end")
+        }
+
+        myJob.cancel()
+
+    }
+
+}
