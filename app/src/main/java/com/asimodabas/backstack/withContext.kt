547558c1bf8b4e2ca0d7eee71b517cd1d2da5603
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-21 18:10:52 +0300

    coroutines withContext

---
+++ app/src/main/java/com/asimodabas/backstack/withContext.kt
@@ -0,0 +1,19 @@
+package com.asimodabas.backstack
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+fun main(){
+
+    runBlocking {
+        launch(Dispatchers.Default) {
+            println("Context: ${coroutineContext}")
+            withContext(Dispatchers.IO){
+                println("Context: ${coroutineContext}")
+            }
+        }
+    }
+
+}
